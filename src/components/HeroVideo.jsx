cd65a9ba1e3075e45f7d718ea7f840feb4fb18bf
Diego Cisneros <dcisnerosg28@gmail.com>
2024-11-27 15:55:07 +0100

    footer and hero video

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -1,7 +1,34 @@
-function HeroVideo() {
-    return (
-        <h2>Hero Video here</h2>
-    )
-}
+import video from "../assets/Video.mp4"
+import "./HeroVideo.css";
+import arrowSvg from "../assets/ARROW.svg"
 
-export default HeroVideo;
+const HeroVideo = () => {
+    const handleScroll = () => {
+      window.scrollTo({
+        top: window.innerHeight,
+        behavior: "smooth",
+      });
+    };
+  
+    return (
+      <div className="hero-container">
+
+        <video
+          className="hero-video"
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        <button className="scroll-button" onClick={handleScroll}>
+          <img src={arrowSvg} alt="Scroll down" />
+        </button>
+      </div>
+    );
+  };
+  
+  export default HeroVideo;
